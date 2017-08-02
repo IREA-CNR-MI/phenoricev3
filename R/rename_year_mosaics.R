d4bedfd50c41158e9edd636399455356c4ea4e02
lbusett <lbusett@gmail.com>
2017-08-02 13:56:24 +0200

    postprocessing R scripts

--- R/rename_year_mosaics.R
+++ R/rename_year_mosaics.R
@@ -1,5 +1,21 @@
-rename_year_mosaics <- function(mosaics_folder) {
+#' @title FUNCTION_TITLE
+#' @description FUNCTION_DESCRIPTION
+#' @param mosaics_folder PARAM_DESCRIPTION
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @rdname rename_year_mosaics
+#' @export
+#' @author Lorenzo Busetto, phD (2017) <lbusett@gmail.com>
 
+rename_year_mosaics <- function(mosaics_folder, out_folder) {
+
+  dir.create(out_folder, recursive = TRUE, showWarnings = FALSE)
   #   ____________________________________________________________________________
   #   change band names                         ####
 
@@ -9,14 +25,10 @@ rename_year_mosaics <- function(mosaics_folder) {
   for (pat in seq_along(patterns)) {
     old_names <- list.files(mosaics_folder, pattern = glob2rx(patterns[pat]), full.names = F)
     new_names <- gsub(strsplit(patterns[pat],"*")[[1]][1], newpatterns[pat], old_names)
-    file.rename(file.path(mosaics_folder, old_names), file.path(mosaics_folder, new_names))
+    file.rename(file.path(mosaics_folder, old_names), file.path(out_folder, new_names))
   }
 }
-
-# mosaics_folder <- "/home/lb/my_data/prasia/mosaics/by_year"
-# rename_year_mosaics(mosaics_folder)
-# temp_vrt <- tempfile(fileext = ".vrt")
-# pp = gdalUtils::gdalbuildvrt(ordered_files, temp_vrt, separate = T)
 #
-#
-#  gdal_translate -ot Int16 -of Gtiff -co "COMPRESS=DEFLATE" /tmp/RtmpyrONRv/file7360614d92a6.vrt /tmp/RtmpyrONRv/prova.tif
+#  mosaics_folder <- "/home/lb/my_data/prasia/mosaics/by_year/2017-07-22/"
+#  out_folder <- "/home/lb/my_data/prasia/mosaics/by_year"
+#  rename_year_mosaics(mosaics_folder, out_folder)
